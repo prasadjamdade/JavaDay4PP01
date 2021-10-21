af7fd5b44db56e648b8d3f97e84a968dc0d10d38
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-21 10:32:41 +0530

    Initialization

---
+++ src/com/day4PracticeProblems/WelcomeDisplay.java
@@ -0,0 +1,12 @@
+package com.day4PracticeProblems;
+
+public class WelcomeDisplay {
+    public static void main(String[] args) {
+        display();
+    }
+
+    private static void display() {
+        System.out.println("Welcome to Snake-Ladder Program.");
+    }
+}
+
