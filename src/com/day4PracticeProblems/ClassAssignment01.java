52dac4b5149b22ed41e0f30dff5b5a3db0b400d4
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-21 23:29:49 +0530

    added function generating palindrome number with user input

--- src/com/day4PracticeProblems/ClassAssignment01.java
+++ src/com/day4PracticeProblems/ClassAssignment01.java
@@ -5,10 +5,10 @@ public class ClassAssignment01 {
 
     public long palindromeInput;
     public long sumPal;
-//    public int fibonacciInput;
+    public int fibonacciInput;
+    public int pal2Input;
 
-
-    public void palindrome(){
+    public void palindromeTester(){
         Scanner scForAll = new Scanner(System.in);
         System.out.println("Please enter the number to check.");
         this.palindromeInput = scForAll.nextLong();
@@ -16,8 +16,9 @@ public class ClassAssignment01 {
 
         while(reversed>0) {
             long remainder = reversed % 10;  //getting remainder
-            this.sumPal = (this.sumPal * 10) + remainder;
-            reversed = reversed / 10;
+            this.sumPal = (this.sumPal * 10) + remainder;  //generating reversed string digit by digit
+            reversed = reversed / 10; //reducing main number to reverse each digit
+
         }
 
           if(palindromeInput == this.sumPal)
@@ -26,9 +27,9 @@ public class ClassAssignment01 {
                   System.out.println("not palindrome");
     }
 
-//    public void fibonacci(){
-//        System.out.println("Fibonacci Yet to be completed.");
-//    }
+    public void fibonacci(){
+        System.out.println("Fibonacci Yet to be completed.");
+    }
 
     public void factorial(int factInput){
         int i = 1;
@@ -40,23 +41,49 @@ public class ClassAssignment01 {
         System.out.println("Factorial of "+factInput+" is "+factOutput+".");
     }
 
-//    public void bubbleSort(){
-//        System.out.println("BubbleSort Yet to be completed.");
-//    }
+    public void bubbleSort(){
+        System.out.println("BubbleSort Yet to be completed.");
+    }
 
     public static void main(String[] args) {
         System.out.println("Start Of Main Method.");
+
         ClassAssignment01 rv = new ClassAssignment01();
-        rv.palindrome();
-//        rv.fibonacci();
-//        rv.bubbleSort();
-        Scanner factScan = new Scanner(System.in);
+        rv.palindromeTester();
+        rv.fibonacci();
+        rv.bubbleSort();
+
+        //generic scanner object
+        Scanner scan = new Scanner(System.in);
+
+        //Factorial Function input
         System.out.println("Enter number to Find Factorial for.");
-        int factInput = factScan.nextInt();
+        int factInput = scan.nextInt();
         rv.factorial(factInput);
-        System.out.println("End Of Main Method.");
+
+        System.out.println("Please Enter number of digits you want to print palindrome chain for.");
+        int palindrome2Input = scan.nextInt();
+        rv.palindrome2Input(palindrome2Input);
+
+        System.out.println("\nEnd Of Main Method.");
     }
 
+    private  void palindrome2Input(int inputToPrintPalindromeSeries) {
 
+        this.pal2Input = inputToPrintPalindromeSeries;
+
+        for (int i=1; i<=pal2Input/2;i++){
+            System.out.print(i);
+        }
+
+        if (inputToPrintPalindromeSeries%2 != 0){
+            System.out.print((inputToPrintPalindromeSeries/2)+1);
+        }
+
+        for (int j=pal2Input/2; j>0;j--) {
+            System.out.print(j);
+        }
+
+    }
 }
 
