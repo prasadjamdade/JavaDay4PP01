f443f77407141bd2c863caa4b870e4691920310b
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-22 11:45:12 +0530

    Added code to set Initial position of player 01 as "0", once the player gets 6 on dice player can get out of 0.

--- src/com/day4PracticeProblems/SinglePlayerInit.java
+++ src/com/day4PracticeProblems/SinglePlayerInit.java
@@ -7,6 +7,11 @@ public class SinglePlayerInit {
 
     private static void display() {
         System.out.println("Welcome to Snake-Ladder Program.");
+        int startPosition = 0; //for all players
+
+        //initializing player no.01 's position
+        int player1Position = startPosition;
+        System.out.println("Position of player No. 01 is: --"+player1Position+"--");
     }
 }
 
