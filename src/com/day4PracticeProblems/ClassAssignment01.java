35cf897c3130960689dff8cdf9dbc28b969eeb68
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-23 02:22:46 +0530

    Added code for fibonacci series print.

--- src/com/day4PracticeProblems/ClassAssignment01.java
+++ src/com/day4PracticeProblems/ClassAssignment01.java
@@ -5,7 +5,7 @@ public class ClassAssignment01 {
 
     public long palindromeInput;
     public long sumPal;
-//    public int fibonacciInput;
+    public int fibonacciInput;
     public int pal2Input;
 
     public void palindromeTest(){
@@ -28,7 +28,21 @@ public class ClassAssignment01 {
     }
 
     public void fibonacci(){
-        System.out.println("\nFibonacci Yet to be completed.");
+        Scanner fibonacciLengthInput = new Scanner(System.in);
+        System.out.println("Please enter the length of Fibonacci Series want to print for.");
+        this.fibonacciInput =  fibonacciLengthInput.nextInt();
+
+        int number01 = 0;
+        int number02 = 1;
+        System.out.print(number01 + " + " + number02 + " + ");
+
+        for(int i=0;i < fibonacciInput-2;i++){
+
+            int number03 = number01 + number02;
+            System.out.print(" + "+number03);
+            number01 = number02;
+            number02 =  number03;
+        }
     }
 
     public void factorial(){
