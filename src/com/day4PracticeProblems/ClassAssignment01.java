63c770f8855c1c562c45f065a61d856846c44db0
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-22 09:35:46 +0530

    Refactored for smart switch.

--- src/com/day4PracticeProblems/ClassAssignment01.java
+++ src/com/day4PracticeProblems/ClassAssignment01.java
@@ -5,10 +5,10 @@ public class ClassAssignment01 {
 
     public long palindromeInput;
     public long sumPal;
-    public int fibonacciInput;
+//    public int fibonacciInput;
     public int pal2Input;
 
-    public void palindromeTester(){
+    public void palindromeTest(){
         Scanner scForAll = new Scanner(System.in);
         System.out.println("Please enter the number to check.");
         this.palindromeInput = scForAll.nextLong();
@@ -28,61 +28,69 @@ public class ClassAssignment01 {
     }
 
     public void fibonacci(){
-        System.out.println("Fibonacci Yet to be completed.");
+        System.out.println("\nFibonacci Yet to be completed.");
     }
 
-    public void factorial(int factInput){
+    public void factorial(){
+        Scanner scanFact = new Scanner(System.in);
+        System.out.println("\nEnter number to Find Factorial for.");
+        int factInput = scanFact.nextInt();
         int i = 1;
         long factOutput = 1;
         while( i <= factInput){
                 factOutput = factOutput*i;
                 i++;
         }
-        System.out.println("Factorial of "+factInput+" is "+factOutput+".");
+        System.out.println("Factorial of "+factInput+" is "+factOutput+".\n");
     }
 
     public void bubbleSort(){
-        System.out.println("BubbleSort Yet to be completed.");
+        System.out.println("\nBubbleSort Yet to be completed.\n");
     }
 
     public static void main(String[] args) {
         System.out.println("Start Of Main Method.");
 
         ClassAssignment01 rv = new ClassAssignment01();
-        rv.palindromeTester();
-        rv.fibonacci();
-        rv.bubbleSort();
+        Scanner methodSelectScan = new Scanner(System.in);
+        System.out.println("""
+                Please select operation from following options.
+                1.Palindrome Number Test
+                2.Fibonacci series
+                3.Sort input number sequence (Bubble-sort)
+                4.Factorial
+                5.Palindrome number generator.
+                6.All""");
 
-        //generic scanner object
-        Scanner scan = new Scanner(System.in);
+        int methodSelectInput = methodSelectScan.nextInt();
 
-        //Factorial Function input
-        System.out.println("Enter number to Find Factorial for.");
-        int factInput = scan.nextInt();
-        rv.factorial(factInput);
 
-        System.out.println("Please Enter number of digits you want to print palindrome chain for.");
-        int palindrome2Input = scan.nextInt();
-        rv.palindrome2Input(palindrome2Input);
-
+        switch (methodSelectInput) {
+            case 1 -> rv.palindromeTest();
+            case 2 -> rv.fibonacci();
+            case 3 -> rv.bubbleSort();
+            case 4 -> rv.factorial();
+            case 5 -> rv.palindromePrint();
+            default -> {
+                rv.palindromeTest();
+                rv.fibonacci();
+                rv.bubbleSort();
+                rv.factorial();
+                rv.palindromePrint();
+            }
+        }
         System.out.println("\nEnd Of Main Method.");
     }
 
-    private  void palindrome2Input(int inputToPrintPalindromeSeries) {
+    private  void palindromePrint() {
 
-        this.pal2Input = inputToPrintPalindromeSeries;
-
-        for (int i=1; i<=pal2Input/2;i++){
-            System.out.print(i);
-        }
-
-        if (inputToPrintPalindromeSeries%2 != 0){
-            System.out.print((inputToPrintPalindromeSeries/2)+1);
-        }
-
-        for (int j=pal2Input/2; j>0;j--) {
-            System.out.print(j);
-        }
+        Scanner scanPal2 = new Scanner(System.in);
+        System.out.println("\nPlease Enter number of digits you want to print palindrome chain for.");
+        int palindrome2Input = scanPal2.nextInt();
+        this.pal2Input = palindrome2Input;
+        for (int i=1; i<=pal2Input/2;i++){System.out.print(i);}
+        if (palindrome2Input%2 != 0){System.out.print((palindrome2Input/2)+1);}
+        for (int j=pal2Input/2; j>0;j--) {System.out.print(j);}
 
     }
 }
