40ea503468d26777331effe91fab903fac448163
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-23 12:27:35 +0530

    Refactored code for Single player to roll a die and break 0th position by having number 6 on die face.

--- src/com/day4PracticeProblems/DiceRollSinglePlayer.java
+++ src/com/day4PracticeProblems/DiceRollSinglePlayer.java
@@ -18,22 +18,15 @@ public class DiceRollSinglePlayer {
         System.out.println("Position of player No. 01 is: --" + player1Position + "--");
         int dieRollCounterP01 = 0; //dice roll counter
 
-        while (player1Position <= 100) {
+        while (player1Position < 6) {
             int rolledDice = (int) (6.0 * Math.random() + 1);  //Dice Generator
             dieRollCounterP01++;
-
             System.out.println("Dice Output is " + rolledDice + " & roll count is " + dieRollCounterP01 + "=> Player's Position was: " + player1Position);
-
             if (player1Position == 0) {
                 if (rolledDice == 6) {
                     player1Position = 6;
                 } else
                     player1Position = startPosition;
-            } else if (player1Position > 94) {
-                if ((100 - player1Position) == rolledDice) {
-                    player1Position += rolledDice;
-                    break;
-                }
             } else
                 player1Position += rolledDice;
         }
