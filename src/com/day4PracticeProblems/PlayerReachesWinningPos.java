29293d554aba3719fe871811a319e48f34d8b0cb
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-23 13:36:02 +0530

    Refactored previous version to let the player reach winning position successfully

---
+++ src/com/day4PracticeProblems/PlayerReachesWinningPos.java
@@ -0,0 +1,66 @@
+package com.day4PracticeProblems;
+
+
+public class PlayerReachesWinningPos {
+
+    int startPosition = 0;
+    public static void main(String[] args) {
+        System.out.println("""
+                =====================================
+                ...Welcome to Snake-Ladder Program...
+                =====================================
+                
+                """);
+        PlayerReachesWinningPos referMethods = new PlayerReachesWinningPos();
+        referMethods.GameStart();
+        System.out.println("""
+                
+                ========================================
+                ...Thank You for playing Snake-Ladder...
+                ========================================
+                """);
+    }
+
+    public void GameStart() {
+
+        int player1Position = startPosition;
+        int dieRollCounterP01 = 0;
+
+        while (player1Position <= 100) {
+            int rolledDice = (int) (6.0 * Math.random() + 1);  //Dice Generator
+            dieRollCounterP01++;
+
+            System.out.println("Dice Output is " + rolledDice + " & roll count is " + dieRollCounterP01 + " => Player's Position was: " + player1Position);
+
+            if (player1Position == 0) {
+                if (rolledDice == 6) {
+                    player1Position = 6;
+                } else
+                    player1Position = startPosition;
+            }
+            else {
+                int movementCase = (int) (4.0 * Math.random() + 1); //Move option
+                switch (movementCase) {
+                    case 1 -> System.out.println("\n*****No move*****.\n");
+                    case 2 -> {
+                        if ((player1Position - rolledDice) < 0) {
+                            player1Position = startPosition;
+                        } else
+                            player1Position -= rolledDice;
+                        System.out.println("\nSnake Bite...: " + "Player's position is " + player1Position+"\n");
+                    }
+                    default -> {
+                        player1Position += rolledDice;
+                        System.out.println("\nLadder jump...: " + "Player's position is " + player1Position + "\n");
+                    }
+                }
+            }
+        }
+
+        System.out.println("Players Position is: " + player1Position + " & die rolled " + dieRollCounterP01 + " times.");
+
+    }
+
+
+}
+
