d34cb7a1e4f42aaa45506dfabdd2df6db64501ca
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-23 12:15:23 +0530

    Refactored for indentation adjustments.

--- src/com/day4PracticeProblems/SinglePlayerInit.java
+++ src/com/day4PracticeProblems/SinglePlayerInit.java
@@ -1,17 +1,19 @@
 package com.day4PracticeProblems;
 
 public class SinglePlayerInit {
+    int startPosition = 0; //for all players
+
     public static void main(String[] args) {
-        display();
+        SinglePlayerInit refVariable = new SinglePlayerInit();
+        refVariable.display();
     }
 
-    private static void display() {
-        System.out.println("Welcome to Snake-Ladder Program.");
-        int startPosition = 0; //for all players
-
-        //initializing player no.01 's position
-        int player1Position = startPosition;
-        System.out.println("Position of player No. 01 is: --"+player1Position+"--");
+    private void display() {
+        System.out.println("""
+                ...Welcome to Snake-Ladder Program...
+                =====================================
+                """);
+        int player1Position = this.startPosition; //initializing player no.01 's position
+        System.out.println("Position of player No. 01 is: --" + player1Position + "--");
     }
 }
-
